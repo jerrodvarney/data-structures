0f23655a44c17779992090a6ed698d8fd07265ca
Jerrod Varney <jerrodvarney@gmail.com>
2022-12-14 15:20:49 -0800

    pass tests for functional shared instantiation pattern in part 1 of sprint.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,37 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    storage: {}
+  };
+
+  _.extend(someInstance, queueMethods);
+
+  return someInstance;
 };
+
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.storage[Object.keys(this.storage).length + 1] = value;
+};
 
+queueMethods.dequeue = function() {
+  var temp = this.storage[1];
+
+  for (var key in this.storage) {
+
+    if (key - 1 === 0) {
+      delete this.storage[key];
+      continue;
+    }
+
+    this.storage[key - 1] = this.storage[key];
+    delete this.storage[key];
+  }
+  console.log(this.storage);
+  return temp;
+};
+
+queueMethods.size = function() {
+  return Object.keys(this.storage).length;
+};
