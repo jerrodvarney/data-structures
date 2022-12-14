1a11d0ccb3c3ad5e35a776daed0c3e3afe66924f
Jerrod Varney <jerrodvarney@gmail.com>
2022-12-14 12:33:37 -0800

    pass all tests for queue and stack using functional instantiation pattern.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,29 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[Object.keys(storage).length + 1] = value;
   };
 
   someInstance.dequeue = function() {
+    var temp = storage[1];
+
+    for (var key in storage) {
+
+      if (key - 1 === 0) {
+        delete storage[key];
+        continue;
+      }
+
+      storage[key - 1] = storage[key];
+      delete storage[key];
+
+    }
+
+    return temp;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,20 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    //add a key value pair to storage at the next number in sequence
+    storage[Object.keys(storage).length + 1] = value;
   };
 
   someInstance.pop = function() {
+    //store value to be delete so it can be returned
+    var temp = storage[Object.keys(storage).length];
+    delete storage[Object.keys(storage).length];
+
+    return temp;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
