94e55f783cf1b87edb7b0c20e6d14bcf221a8d7b
Jerrod Varney <jerrodvarney@gmail.com>
2022-12-14 15:47:02 -0800

    pass all tests for the prototypal instantiation pattern.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+
+  return someInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.storage[Object.keys(this.storage).length + 1] = value;
+};
 
+queueMethods.dequeue = function() {
+  var temp = this.storage[1];
+
+  for ( var key in this.storage) {
+
+    if (key - 1 === 0) {
+      delete this.storage[key];
+      continue;
+    }
+
+    this.storage[key - 1] = this.storage[key];
+    delete this.storage[key];
+  }
+
+  return temp;
+};
+
+queueMethods.size = function() {
+  return Object.keys(this.storage).length;
+};
