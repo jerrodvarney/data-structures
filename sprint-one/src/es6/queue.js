43de828276ceeeb16db65e990840a20c8f903092
Jerrod Varney <jerrodvarney@gmail.com>
2022-12-14 16:19:56 -0800

    pass all tests in ES6 instantiation pattern.

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,33 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  enqueue(value) {
+    this.storage[Object.keys(this.storage).length + 1] = value;
+  }
+
+  dequeue() {
+    var temp = this.storage[1];
+
+    for (var key in this.storage) {
+
+      if (key - 1 === 0) {
+        delete this.storage[key];
+        continue;
+      }
+
+      this.storage[key - 1] = this.storage[key];
+      delete this.storage[key];
+    }
+
+    return temp;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
+
+var test = new Queue();
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,24 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  push(value) {
+    this.storage[Object.keys(this.storage).length + 1] = value;
+  }
+
+  pop() {
+    var temp = this.storage[Object.keys(this.storage).length];
+
+    delete this.storage[Object.keys(this.storage).length];
+
+    return temp;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
+
+var test = new Stack();
