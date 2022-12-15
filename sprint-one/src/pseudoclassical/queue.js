2b101ae8e08cb0cae96936fc3e8104a206eee771
Jerrod Varney <jerrodvarney@gmail.com>
2022-12-14 16:01:03 -0800

    pass all tests for pseudoclassical instantiation pattern

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,28 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[ Object.keys(this.storage).length + 1] = value;
+};
 
+Queue.prototype.dequeue = function() {
+  var temp = this.storage[1];
+
+  for (var key in this.storage) {
+
+    if (key - 1 === 0) {
+      delete this.storage[1];
+      continue;
+    }
+
+    this.storage[key - 1] = this.storage[key];
+    delete this.storage[key];
+  }
+
+  return temp;
+};
+
+Queue.prototype.size = function() {
+  return Object.keys(this.storage).length;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,19 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
 
+Stack.prototype.push = function(value) {
+  this.storage[Object.keys(this.storage).length + 1] = value;
+};
 
+Stack.prototype.pop = function() {
+  var temp = this.storage[Object.keys(this.storage).length];
+
+  delete this.storage[Object.keys(this.storage).length];
+
+  return temp;
+};
+
+Stack.prototype.size = function() {
+  return Object.keys(this.storage).length;
+};
