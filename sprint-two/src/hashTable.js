7292dc7eaf526cd1a2e534dcf7375d3174679107
Jerrod Varney <jerrodvarney@gmail.com>
2022-12-16 15:32:56 -0800

    pass tests in hashTable.js. Pushed data before saving code changes last time.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -9,44 +9,29 @@ HashTable.prototype.insert = function(k, v) {
   var bucket = this._storage.get(index) === undefined ? [] : this._storage.get(index);
   var tuple = [k, v];
 
-  //Reassignment if key already exists (t = tuple)
+  //Reassignment if key alreadyexists t = tuple
   bucket.forEach((t) => {
-
     if (t[0] === k) {
       t[1] = v;
       this._storage.set(index, bucket);
-
       return;
     }
   });
 
   //Otherwise it pushes new tuple into bucket
   bucket.push(tuple);
-
-  var bucket = this._storage.get(index);
-
-  for (var tuple in bucket) {
-
-    if ((bucket[tuple].indexOf(k) === 0)) {
-
-      return bucket[tuple][1];
-    }
-  }
+  this._storage.set(index, bucket);
 };
+
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-
   var bucket = this._storage.get(index);
-
   for (var tuple in bucket) {
-
     if ((bucket[tuple].indexOf(k) === 0)) {
-
       return bucket[tuple][1];
     }
   }
-
 };
 
 HashTable.prototype.remove = function(k) {
@@ -56,21 +41,18 @@ HashTable.prototype.remove = function(k) {
 
   //If a tuple in this bucket contains the key, delete that tuple array
   for (var i in bucket) {
-
     if (bucket[i][0] === k) {
       delete bucket[i];
     }
-
     //How to remove bucket if it has no tuples???
     if (bucket.length === 0) {
       this._storage.set(index, undefined);
-
       return;
     }
   }
-
   this._storage.set(index, bucket);
-};
+};art
+
 
 
 
