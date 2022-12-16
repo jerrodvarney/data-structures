e80ddd5bb8a63d90d07ebeab12d53653818d940b
Jerrod Varney <jerrodvarney@gmail.com>
2022-12-15 20:48:22 -0800

    pass tests for set.js in sprint part 2

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,19 +1,31 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (this.storage.indexOf(item) !== -1) {
+    return;
+  }
+  this.storage.push(item);
 };
 
 setPrototype.contains = function(item) {
+  for (var index in this.storage) {
+    if (this.storage[index] === item) { return true; }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  if (this.storage.indexOf(item) !== -1) {
+    delete this.storage[this.storage.indexOf(item)];
+  }
 };
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
